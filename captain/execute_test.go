63fe81f10c5c8567f9dfb9aa08c2e2b5a77e72e6
spiddy <d.kapanidis@gmail.com>
2015-05-24 01:52:59 +0200

    added execute_test

---
+++ captain/execute_test.go
@@ -0,0 +1,22 @@
+package captain // import "github.com/harbur/captain/captain"
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecute(t *testing.T) {
+	res := execute("echo", "testing")
+	assert.Equal(t, nil,res, "it should execute without errors");
+}
+
+func TestOneliner(t *testing.T) {
+	res := oneliner("echo", "testing")
+    assert.Equal(t, "testing", res, "it should return the trimmed result");
+}
+
+func TestOnelinerTrimmed(t *testing.T) {
+	res := oneliner("echo", "testing with spaces  ")
+    assert.Equal(t, "testing with spaces", res, "it should return the trimmed result");
+}
