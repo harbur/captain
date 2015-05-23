2493070408a59e0208bc38fe560ec9ba469d13ab
spiddy <d.kapanidis@gmail.com>
2015-05-24 01:41:07 +0200

    added print_test

---
+++ captain/print_test.go
@@ -0,0 +1,18 @@
+package captain // import "github.com/harbur/captain/captain"
+
+import (
+	"testing"
+)
+
+func TestPrintInfo(t *testing.T) {
+	info("test info %s", "message")
+}
+
+func TestPrintErr(t *testing.T) {
+	err("test err %s", "message")
+}
+
+func TestPrintDebug(t *testing.T) {
+	options.debug=true
+	debug("test debug %s", "message")
+}
