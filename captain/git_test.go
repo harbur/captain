f9e5d37907bd1d25f3563c3e4c96bce150bf4127
spiddy <d.kapanidis@gmail.com>
2015-05-24 12:06:43 +0200

    Added git tests

--- captain/git_test.go
+++ captain/git_test.go
@@ -6,6 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGitGetRevision(t *testing.T) {
+	assert.Equal(t, 7, len(getRevision()), "they should be equal")
+}
+
+func TestGitGetBranch(t *testing.T) {
+	assert.Equal(t, "master", getBranch(), "they should be equal")
+}
+
 func TestGitIsDirty(t *testing.T) {
 	assert.Equal(t, false, isDirty(), "they should be equal")
+}
+
+func TestGitIsGit(t *testing.T) {
+	assert.Equal(t, true, isGit(), "they should be equal")
 }
