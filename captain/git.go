7b3bd1cd10d58d6dcc411ff192730ad41f1872aa
spiddy <d.kapanidis@gmail.com>
2015-06-12 14:46:28 +0200

    Fixes #5 Add mapping of '/' char to '.' char whne translating from git tags to docker tags

--- captain/git.go
+++ captain/git.go
@@ -1,11 +1,17 @@
 package captain // import "github.com/harbur/captain/captain"
 
+import (
+    "strings"
+)
+
 func getRevision() string {
 	return oneliner("git", "rev-parse", "--short", "HEAD")
 }
 
 func getBranch() string {
-	return oneliner("git", "rev-parse", "--abbrev-ref", "HEAD")
+	branch := oneliner("git", "rev-parse", "--abbrev-ref", "HEAD")
+	branch = strings.Replace(branch, "/", ".", -1)
+	return branch
 }
 
 func isDirty() bool {
