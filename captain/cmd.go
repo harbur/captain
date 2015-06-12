8e3307f2aadc0b24a0a3b3192607b042b301b7d2
spiddy <d.kapanidis@gmail.com>
2015-06-12 15:13:06 +0200

    Preparing version 0.0.2

--- captain/cmd.go
+++ captain/cmd.go
@@ -67,7 +67,7 @@ func handleCmd() {
 		Short: "Display version",
 		Long:  `Displays the version of Crane.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v0.0.1")
+			fmt.Println("v0.0.2")
 		},
 	}
 
