2024ef075c545b712afa79669e75b820005eb8d6
Dimitris Kapanidis <d.kapanidis@gmail.com>
2015-07-08 13:43:38 +0200

    fix release link

--- release.sh
+++ release.sh
@@ -14,7 +14,7 @@ go test ./...
 
 echo "Update version..."
 sed -i.bak 's/fmt\.Println("v[0-9]*\.[0-9]*\.[0-9]*")/fmt.Println("v'$version'")/' captain/cmd.go
-sed -i.bak 's/captain\/releases\/download\/v[0-9]*\.[0-9]*\.[0-9]*\/captain/"captain\/releases\/download\/v'$version'\/captain/' README.md
+sed -i.bak 's/captain\/releases\/download\/v[0-9]*\.[0-9]*\.[0-9]*\/captain/captain\/releases\/download\/v'$version'\/captain/' README.md
 rm captain/cmd.go.bak README.md.bak
 
 echo "Build binary..."
