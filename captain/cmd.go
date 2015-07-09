e1f65c64914286e340f9a657500a4a8f81935840
Dimitris Kapanidis <d.kapanidis@gmail.com>
2015-07-09 18:25:02 +0200

    Preparing version 0.4.0

--- captain/cmd.go
+++ captain/cmd.go
@@ -91,7 +91,7 @@ func handleCmd() {
 		Short: "Display version",
 		Long:  `Displays the version of Captain.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("v0.3.2")
+			fmt.Println("v0.4.0")
 		},
 	}
 
