e60f3eb3bb59c7fe13079966f5ca0e0bc126805a
Eric J. Holmes <eric@ejholmes.net>
2015-07-23 16:52:40 -0700

    Change package structure.

---
+++ cmd/captain/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/harbur/captain"
+)
+
+func main() {
+	captain.RealMain()
+}
--- git.go
+++ git.go
@@ -1,4 +1,4 @@
-package captain // import "github.com/harbur/captain/captain"
+package captain // import "github.com/harbur/captain"
 
 import (
 	"regexp"
