3ccdbb21d7631bdca3b20f02a52d16b2f6c548b2
spiddy <dimitris.kapanidis@harbur.io>
2016-05-24 13:26:36 +0200

    Added captain.Post() tests

--- captain_test.go
+++ captain_test.go
@@ -21,3 +21,20 @@ func TestPreFail(t *testing.T) {
 	res := Pre(app)
 	assert.NotNil(t, res, "Error returned")
 }
+
+func TestPost(t *testing.T) {
+	app := App{
+		Post: []string{"echo test"},
+	}
+	res := Post(app)
+
+	assert.Nil(t, res, "No error returned")
+}
+
+func TestPostFail(t *testing.T) {
+	app := App{
+		Post: []string{"nonexistingCommand"},
+	}
+	res := Post(app)
+	assert.NotNil(t, res, "Error returned")
+}
